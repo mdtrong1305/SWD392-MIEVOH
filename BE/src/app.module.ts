3e98e7c493989308b2ff855a620dc8f87de04edb
trlamm1505 <tqlam150504@gmail.com>
2026-06-02 22:44:05 +0700

    chore: remove BE folder from feature/light-dark-mode branch

--- BE/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './modules-system/prisma/prisma.module';
-import { TokenModule } from './modules-system/token/token.module';
-import { AuthModule } from './modules-api/auth/auth.module';
-
-@Module({
-  imports: [PrismaModule, TokenModule, AuthModule],
-})
-export class AppModule {}
--- BE/src/common/decorators/role.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const IS_ROLE = 'isRole';
-
-export type RoleType = 'ADMIN' | 'CUSTOMER';
-
-export const Role = (roleType: RoleType) => SetMetadata(IS_ROLE, roleType);
