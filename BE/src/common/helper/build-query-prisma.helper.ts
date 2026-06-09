7f2e72ae7c9605c34cfc7c91e8c21a84731d4c14
DaShork <nhatminhvo2311@gmail.com>
2026-06-09 15:22:06 +0700

    sửa lại build query thêm các trường filters

--- BE/src/common/helper/build-query-prisma.helper.ts
+++ BE/src/common/helper/build-query-prisma.helper.ts
@@ -8,9 +8,23 @@ export function buildQueryPrisma(query) {
   }
   // xử lý filters
   for (const [key, value] of Object.entries(filters)) {
-    if (typeof value === "string") {
+    // 1. Nếu field là Mảng (genres, favoriteGenres)
+    if (key === 'genres' || key === 'favoriteGenres') {
+      if (Array.isArray(value)) {
+        filters[key] = { hasSome: value }; // Tìm phim có CHỨA BẤT KỲ thể loại nào trong mảng truyền lên
+      } else {
+        filters[key] = { has: value }; // Tìm phim có CHỨA chính xác thể loại này
+      }
+    }
+    // 2. Nếu field là ID (id, Id) hoặc status, type thì giữ nguyên (Tìm chính xác)
+    else if (key.endsWith('Id') || key === '_id' || key === 'status' || key === 'userType') {
+      filters[key] = value;
+    }
+    // 3. Các trường Text còn lại (title, name, cast...) thì tìm kiếm mờ (Like / Contains)
+    else if (typeof value === "string") {
       filters[key] = {
         contains: value,
+        mode: 'insensitive',
       };
     }
   }
